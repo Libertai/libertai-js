0ccdb1829dd73a30b383f23fa1a5a16c4991525d
amiller68 <alexanderscottmiller@gmail.com>
2024-04-02 18:43:06 -0400

    fix: basic exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { KnowledgeStore } from './knowledge-store';
 export { LlamaCppApiEngine } from './inference';
+export { Model, Chat, PartialChat } from './types';
+export { Agent } from './agent';
